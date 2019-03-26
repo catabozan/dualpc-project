7ef330a9afeffdd5c1794ceedfc814eab07fca78
catabozan <catabozan@protonmail.com>
2019-03-26 19:56:02 +0200

    added location button, more images on index and added transparent dark bg to index text

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var imagesInFolder = 2; //how many images are in /resources/images/index
+var imagesInFolder = 7; //how many images are in /resources/images/index
 
 var body = document.querySelector("body");
 var sibiu = document.querySelector("#sibiu");
-var imageCounter = 2;
+var imageCounter = 2; //uncomment to have ordered images on index
 
 sibiu.addEventListener("click", function(){
 	//console.log(imageCounter);
 	body.style.backgroundImage = "url(\"resources/images/index/" + imageCounter + ".jpg\")";
-	imageCounter++;
-	if(imageCounter > imagesInFolder) imageCounter = 1;
-})
+	imageCounter++; //uncomment to have ordered images on index
+	if(imageCounter > imagesInFolder) imageCounter = 1; //uncomment to have ordered images on index
+});
